e162cac4c42f21d4557a4a86f7f76dfc0c4b28e2
Setinais13 <vynny.cg@gmail.com>
2017-03-31 14:42:13 -0300

    add

--- app/controllers/EstagioController.php
+++ app/controllers/EstagioController.php
@@ -34,5 +34,5 @@ class EstagioController extends \HXPHP\System\Controller
 	{
 		$post = $this->request->post();
 		$this->view->setVars(['request' => $post]);
-	}
+	}g
 }
--- app/models/Vaga.php
+++ app/models/Vaga.php
@@ -11,4 +11,18 @@ class Vaga extends \HXPHP\System\Model
 	static $belongs_to = [
 		['cargo_has_instituicao']
 	];
+
+	public static function search($id_user,$filters,$role)
+	{
+		$vagas = self::find('all', 
+			[ 
+				'conditions' => [ 
+					'cargo_has_instituicao_id = ?',
+					CargoHasInstituicao::find_by_instituicao_id(Instituicao::find_by_usuario_id($id_user)->id)->id
+				]
+			]
+		);
+
+		return $vagas;
+	}
 }
