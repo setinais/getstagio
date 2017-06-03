ef66a2d6b2dba64f226dd92a8398092ea5cbc2e7
lucianoguida1 <lucianoguida10@gmail.com>
2017-06-02 22:35:59 -0300

    Concluir cadastro curriculo - incompleto ainda|

---
+++ app/models/ConhecimentoSistema.php
@@ -0,0 +1,30 @@
+<?php 
+
+class ConhecimentoSistema extends HXPHP\System\Model{
+	static $belongs_to = [
+		['estudante'],
+		['sistema']
+		];
+	public static function cadastrar($idu,$idesc)
+	{
+		$callback = new \stdClass;
+		$callback->status = false;
+		$callback->cadastro = null;
+		$callback->errors = [];
+			$cadastrar = self::create(array('sistema_id'=>$idesc,'estudante_id'=>$idu));
+			if($cadastrar->is_valid())
+			{
+				$callback->status = true;
+				$callback->cadastro = $cadastrar;
+			}
+			else
+			{
+				$errors = $cadastrar->errors->get_raw_errors();
+				foreach ($errors as $campo => $messagem) {
+					array_push($callback->errors, $messagem[0]);
+				}
+			}
+
+		return $callback;
+	}
+}
