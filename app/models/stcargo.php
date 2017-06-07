f4426d4341a1f808697ec3cd26a1dd8f00958781
lucianoguida1 <lucianoguida10@gmail.com>
2017-06-07 19:32:38 -0300

    Envio do site para git

---
+++ app/models/stcargo.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* 
+*/
+class Stcargo extends \HXPHP\System\Model
+{
+	static $has_many = [
+		['estudantes']
+	];
+
+	static $validates_presence_of = [
+			[
+				'nome',
+				'message' => '<strong>Nome do cargo</strong> é um campo obrigatório.'
+			]
+	];
+
+	public static function cadastrar($post)
+	{
+		$callback = new \stdClass;
+		$callback->status = false;
+		$callback->cadastro = null;
+		$callback->errors = [];
+  			
+			$cadastrar = self::create(array(
+				'nome'=>(isset($post['cargo']) && $post['cargo']!="")?$post['cargo']:"",
+				'desc'=>(isset($post['desc']) && $post['desc']!="")?$post['desc']:"",
+				));
+			if($cadastrar->is_valid()){
+				$callback->status = true;
+				$callback->cadastro = $cadastrar;
+			}
+			else
+			{
+				$errors = $cadastrar->errors->get_raw_errors();
+				foreach ($errors as $campo => $messagem) {
+					array_push($callback->errors, $messagem[0]);
+				}
+			}
+
+		return $callback;
+	}
+}
+
